22b22b1214a93e06f75e41eeaffdb7a3630d1c77
gift <geezyenyoghasim@gmail.com>
2021-06-05 18:25:20 +0100

    feat : working on landing page

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,3 @@
-const isEmpty = (arg) => {
-  if (arg === "") {
-    return true;
-  }
-  return false;
-};
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const firstname = document.getElementById("firstname");
-  const lastname = document.getElementById("lastname");
-  const email = document.getElementById("email");
-  const password = document.getElementById("password");
-  const error = document.querySelector(".error");
-
-  const firstnameValue = firstname.value;
-  const lastnameValue = lastname.value;
-  const emailValue = email.value;
-  const passwordvalue = password.value;
-
-  if (
-    isEmpty(firstnameValue) ||
-    isEmpty(lastnameValue) ||
-    isEmpty(emailValue) ||
-    isEmpty(passwordvalue)
-  ) {
-    error.textContent = "please fill in all fields";
-    error.style.display = "block";
-  }
-};
-
-// register button
-const button = document.getElementById("submit");
-button.addEventListener("click", handleSubmit);
+window.addEventListener("scroll", (e) => {
+  //   console.log(window.scrollY);
+});

64778eca97a6c9557dfe4e6c15835e5f0a894f8b
gift <geezyenyoghasim@gmail.com>
2021-06-07 09:46:30 +0100

    feat : added regex for the email validation

--- scripts/register.js
+++ scripts/register.js
@@ -7,25 +7,37 @@ const isEmpty = (arg) => {
 
 const handleSubmit = (e) => {
   e.preventDefault();
+  const error = document.querySelector(".error");
   const firstname = document.getElementById("firstname");
   const lastname = document.getElementById("lastname");
   const email = document.getElementById("email");
   const password = document.getElementById("password");
-  const error = document.querySelector(".error");
-
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   const firstnameValue = firstname.value;
   const lastnameValue = lastname.value;
   const emailValue = email.value;
   const passwordvalue = password.value;
+  error.style.display = "none";
+  error.innerHTML = "";
 
+  const ul = document.createElement("ul");
   if (
     isEmpty(firstnameValue) ||
     isEmpty(lastnameValue) ||
     isEmpty(emailValue) ||
     isEmpty(passwordvalue)
   ) {
-    error.textContent = "please fill in all fields";
+    ul.appendChild(document.createElement("li")).innerText =
+      "Please fill in all fields";
     error.style.display = "block";
+    error.appendChild(ul);
+    return;
+  }
+  if (!emailRegex.test(emailValue)) {
+    ul.appendChild(document.createElement("li")).innerText =
+      "Please input a valid Email adress";
+    error.style.display = "block";
+    error.appendChild(ul);
   }
 };
 
